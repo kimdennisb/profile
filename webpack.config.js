2349c9cd8cef009cdfa257b1d255198aedf1211c
Dennis Kim <kimutaidennisbrian@gmail.com>
2023-02-08 16:21:30 +0300

    Add bundle output folder and webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", ["@babel/preset-react",{"runtime": "automatic"}],
+    ]},
+        },
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"], // ?name=[name].[ext] is only necessary to preserve the original file name
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      filename: "./index.html",
+      favicon: "./public/favicon.ico",
+      manifest: "./public/manifest.json"
+    }),
+  ],
+  mode:"development"
+};
